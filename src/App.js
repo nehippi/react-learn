980e3a4e454ed35a831360f8fe32796d52381322
ztsiarentsyeu <v-ztsiarentsyeu@expediagroup.com>
2022-08-24 14:32:27 +0300

    update render method update fetching refactoring

--- src/App.js
+++ src/App.js
@@ -1,67 +1,71 @@
 import "./App.css";
 import React from "react";
 
+let IMAGE_HEIGHT = 200;
+
 class Image extends React.Component {
     constructor(props) {
         super(props);
-        this.addCat = this.addCat.bind(this);
-        this.handleScroll = this.handleScroll.bind(this);
-        this.imageHeigh = "200";
         this.state = {
-            isFirstTime: true,
+            isFetching: false,
             error: null,
             isLoaded: false,
             images: [],
             catsElements: [],
             makeTimeout: false
         };
-        window.addEventListener("scroll", this.handleScroll);
+
     }
 
     handleScroll = (e) => {
-        const bottom = e.target.scrollingElement.scrollHeight - this.imageHeigh - e.target.scrollingElement.scrollTop <= e.target.scrollingElement.clientHeight + 1;
+        const bottom = e.target.scrollingElement.scrollHeight - IMAGE_HEIGHT - e.target.scrollingElement.scrollTop <= e.target.scrollingElement.clientHeight + 1;
         if (bottom) {
             this.addCat();
         }
     }
 
+    componentDidMount() {
+        this.addCat();
+        window.addEventListener("scroll", this.handleScroll);
+    }
 
-    addCat() {
-        console.log("addCat")
-        fetch("https://api.thecatapi.com/v1/images/search?limit=10")
-            .then((response) => {
-                return response.json();
-            })
-            .then((arrayOfData) => {
-                const buffArray = [];
-                arrayOfData.map((data) => {
-                    console.log(data);
-                    buffArray.push(
-                        <div>
-                            <img src={data.url} width="200" height={this.imageHeigh}/>
-                        </div>
-                    );
-                });
-                this.setState({
-                    images: this.state.images.concat(buffArray)
-                });
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+
+
+    addCat = () => {
+        if (!this.state.isFetching) {
+            console.log("addCat")
+            this.setState({
+                isFetching: true
             });
+            fetch("https://api.thecatapi.com/v1/images/search?limit=10")
+                .then((response) => {
+                    return response.json();
+                })
+                .then((arrayOfData) => {
+                    const buffArray = [];
+                    arrayOfData.forEach((imageData) => {
+                        console.log(imageData);
+                        buffArray.push(imageData);
+                    });
+                    this.setState({
+                        images: this.state.images.concat(buffArray),
+                        isFetching: false
+                    });
+                });
+        }
     }
 
 
     render() {
-        const style = {
-            justifyContent: 'center',
-            alignItems: 'center',
-            position: 'absolute', // Найдите
-        }
-        if (this.state.isFirstTime) {
-            this.addCat()
-            this.setState({
-                isFirstTime: false
-            });
-        }
-        const cats = this.state.images;
+        const cats = this.state.images.map((data) => {
+            return (
+                <div key={data.id}>
+                    <img src={data.url} height={IMAGE_HEIGHT} alt="could not download"/>
+                </div>)
+        });
         return (
             <div>
                 {cats}
